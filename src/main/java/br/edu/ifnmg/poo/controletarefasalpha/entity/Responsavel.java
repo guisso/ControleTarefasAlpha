027b5d99354e732f66a26416e3a722817a1b2e62
guisso <2601390+guisso@users.noreply.github.com>
2020-12-11 17:44:35 -0300

    Entidade Responsavel

---
+++ src/main/java/br/edu/ifnmg/poo/controletarefasalpha/entity/Responsavel.java
@@ -0,0 +1,57 @@
+/* 
+ * Material didático destinado ao curso
+ * de Programação Orientada a Objetos do 
+ * Bacharelado em Ciência da Computação 
+ * do IFNMG - Câmpus Montes Claros
+ */
+package br.edu.ifnmg.poo.controletarefasalpha.entity;
+
+/**
+ *
+ * @author Luis Guisso <luis dot guisso at ifnmg dot edu dot br>
+ */
+public class Responsavel {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+    public Responsavel() {
+    }
+
+    public Responsavel(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+
+}
